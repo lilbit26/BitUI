bb15c575fa0da99c2f43dcb165bc4f8a460105f3
kaiqiang.zhou <kaiqiang.zhou@cn.alps.com>
2021-12-22 17:15:10 +0800

    Update auras

--- modules/auras/auras.lua
+++ modules/auras/auras.lua
@@ -2,6 +2,20 @@ local _, ns = ...
 local Z = ns.Z
 local A = Z:AddModule("Auras")
 
+--[[
+    ls_UI:
+        LSBuffHeader
+        LSDebuffHeader
+]]
+
 function A:Load()
-    -- TODO
+    for _, header in pairs({LSBuffHeader, LSDebuffHeader}) do
+        header:HookScript("OnAttributeChanged", function(_, _, button)
+            local parent = button.TextParent
+
+            parent:ClearAllPoints()
+            parent:SetPoint("TOPLEFT", 1, -1)
+            parent:SetPoint("BOTTOMRIGHT", 1, -1)
+        end)
+    end
 end
